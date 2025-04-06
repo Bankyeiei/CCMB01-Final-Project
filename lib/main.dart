e5431191a63c3292bb5a6292bbfccc6dba4f6148
Bankyeiei <bank23113@gmail.com>
2025-04-06 17:18:09 +0700

    Make the screen vertical only

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'theme/theme.dart' as theme;
@@ -11,6 +12,7 @@ import 'app/modules/login/login_page_binding.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]); //* Make the screen vertical only
   await Firebase.initializeApp();
   runApp(const MainApp());
 }
